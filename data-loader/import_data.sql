47eb2ec579e386c6c0ca698e765fc2dd9a195a15
daprogCo <daprog.co@gmail.com>
2025-11-17 23:38:04 -0500

    Edit to import_data.sql

--- data-loader/import_data.sql
+++ data-loader/import_data.sql
@@ -1,8 +1,11 @@
 USE Cars;
 GO
 
-BULK INSERT Car 
-FROM '/tmp/Automobile.csv' 
-WITH ( FIRSTROW = 2); 
-
+BULK INSERT Car
+FROM '/tmp/Automobile.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A'
+);
 GO
