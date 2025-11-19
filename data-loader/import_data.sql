88a1c8d3ed1a7d3924c10a20c7173cbdb750a731
daprogCo <daprog.co@gmail.com>
2025-11-18 23:04:06 -0500

    Added a staging table

--- data-loader/import_data.sql
+++ data-loader/import_data.sql
@@ -1,14 +1,51 @@
 USE Cars;
 GO
 
-BULK INSERT Car
+-----------------------------------------
+-- 1. BULK INSERT into CarTemp
+-- (schema EXACTLY matches CSV)
+-----------------------------------------
+BULK INSERT CarTemp
 FROM '/tmp/Automobile.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '0x0D0A',
-    TABLOCK
+    ROWTERMINATOR = '0x0D0A',   -- CRLF
+    TABLOCK,
+    KEEPNULLS
 );
 GO
 
+-----------------------------------------
+-- 2. Insert validated/transformed rows 
+--    from staging into final Car table
+-----------------------------------------
+INSERT INTO Car (
+    name,
+    mpg,
+    cylinders,
+    displacement,
+    horsepower,
+    weight,
+    acceleration,
+    model_year,
+    origin
+)
+SELECT
+    name,
+    mpg,
+    cylinders,
+    displacement,
+    horsepower,
+    weight,
+    acceleration,
+    model_year,
+    origin
+FROM CarTemp;
+GO
+
+-----------------------------------------
+-- 3. Delete staging table
+-----------------------------------------
+DROP TABLE CarTemp;
 GO
--- data-loader/init.sql
+++ data-loader/init.sql
@@ -1,23 +1,42 @@
--- Step 1: Create the 'Cars' database
+-----------------------------------------
+-- Create the 'Cars' database
+-----------------------------------------
 CREATE DATABASE Cars;
 GO
 
--- Step 2: Switch context to the new 'Cars' database
 USE Cars;
 GO
 
--- Step 3: Create the 'Car' table
+-----------------------------------------
+-- Final table Car
+-----------------------------------------
 CREATE TABLE Car (
-    Id INT PRIMARY KEY IDENTITY(1,1),
+    Id INT IDENTITY(1,1) PRIMARY KEY,
     name NVARCHAR(255) NOT NULL,
-    mpg DECIMAL(3,1) NOT NULL,
-    cylinders INT NOT NULL,
-    displacement INT NOT NULL,
-    horsepower INT NOT NULL,
-    weight INT NOT NULL,
-    acceleration DECIMAL(3,1) NOT NULL,
-    model_year INT NOT NULL,
-    origin NVARCHAR(255) NOT NULL,
-    is_deleted INT NULL
+    mpg DECIMAL(5,2) NULL,
+    cylinders INT NULL,
+    displacement DECIMAL(5,2) NULL,
+    horsepower INT NULL,
+    weight INT NULL,
+    acceleration DECIMAL(5,2) NULL,
+    model_year INT NULL,
+    origin NVARCHAR(255) NULL,
+    is_deleted BIT NOT NULL DEFAULT 0
+);
+GO
+
+-----------------------------------------
+-- Staging table CarTemp (matches CSV)
+-----------------------------------------
+CREATE TABLE CarTemp (
+    name NVARCHAR(255),
+    mpg FLOAT,
+    cylinders INT,
+    displacement FLOAT,
+    horsepower FLOAT NULL,
+    weight INT,
+    acceleration FLOAT,
+    model_year INT,
+    origin NVARCHAR(255)
 );
 GO
